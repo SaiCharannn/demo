d9d973cb87e0131da2c7c89ba4a8070b261e841f
CHILLAMCHERLA SAI CHARAN <110535975+SaiCharan200@users.noreply.github.com>
2025-11-08 16:58:53 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,37 @@
+# Integer
+# Float
+# Complex number 
+# String
+# Boolean
+num1 = 100
+num2 = 10.2
+num3 = complex(3,2)
+st1 = 'String'
+st2 = "String"
+st3 = """My name is sai Charan""" 
+bool = True
+bool = False
+
+
+
+lst  = [1, 'Charan', 'Piduguralla','Andhra Pradesh', 9.2]
+lst[4] = 9.5
+
+print(lst[4])
+
+
+
+tuple = ("Harsha", "Sunny", "Lucky", "Poojya")
+print(tuple)
+
+set2 = {'lucky','harsha', 'sunny' ,'sunny'}
+print(set2)
+
+
+dict = {'One' : 'Sunny', 2 : 'Harsha', 3 : 'Lucky', 4 : "Poojya"}
+print(dict['One'])
+print(dict.get('One'))
+# list
+# tuple
+# set
+# dictionary
